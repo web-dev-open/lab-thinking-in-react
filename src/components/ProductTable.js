9171eb8a46418f20b4d101325bfeb2e614ed88ce
Levi Milli <107660704+LeviMilli@users.noreply.github.com>
2022-10-18 21:18:36 -0700

    60% done

---
+++ src/components/ProductTable.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ProductRow from './ProductRow'
+
+function ProductTable() {
+  return (
+    <div>
+        <h1>Name</h1>
+        <h2>Price</h2>
+        <ProductRow/>
+
+    </div>
+  )
+}
+
+export default ProductTable
