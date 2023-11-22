1eb791ec4089b754d87fe26b3f6da00fd1297dc5
Shiva Sagar <sagarshiva111555@gmail.com>
2023-11-22 10:07:46 +0530

    solve lab

---
+++ src/components/ProductTable.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import ProductRow from './ProductRow'
+// import './table.css'
+import './table.css'
+
+const ProductTable = ({products}) => {
+  return (
+    <div className='body'>
+    <section className='tabody'>
+      <table>
+      <thead>
+       <tr>
+        <th>Name</th>
+        <th>Price</th>
+        
+       </tr>
+    </thead>
+    <tbody>
+        {products.map((product)=>(
+             
+             <ProductRow productrow={product}/>
+        ))}
+         
+    </tbody>
+      </table>
+    </section>
+    </div>
+  )
+}
+
+export default ProductTable
