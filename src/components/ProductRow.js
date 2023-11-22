5e81deeade0de0cd6effa8aad04a3bc656d479a8
SushmithaSudarshan <sushmithasudarshan515@gmail.com>
2023-11-22 13:04:26 +0530

    done

---
+++ src/components/ProductRow.js
@@ -0,0 +1,16 @@
+// src/components/ProductRow.js
+
+import React from 'react';
+
+function ProductRow({ product }) {
+  const name = product.inStock ? product.name : <span style={{ color: 'red' }}>{product.name}</span>;
+
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{product.price}</td>
+    </tr>
+  );
+}
+
+export default ProductRow;
