268613c89c6360e6b9ec2821de2bd52199284f8c
Sarthak Sundriyal <sarthakvsundriyal@gmail.com>
2023-12-26 10:55:26 +0530

    SarthakSundriyal-internship

---
+++ src/components/ProductsPage.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import jsonData from "../data.json";
+import ProductTable from "./ProductTable";
+import SearchBar from "./SearchBar";
+function ProductsPage() {
+  const [query, setQuery] = useState(''); 
+  const [filterproducts, setFilterProducts] = useState(jsonData);
+  async function handleChange(e) {
+     return await new Promise((resolve)=>{
+      setQuery(e.target.value);
+      resolve(e.target.value);
+    })
+    .then((value)=>{
+        setFilterProducts(jsonData.filter((val)=> val.name.split(' ').some(word=>word.toLowerCase().includes(value))))
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+  }
+  return (
+    <div >
+      <SearchBar dvalue={query} onchange={handleChange}  />
+      <ProductTable products={filterproducts} />
+    </div>
+  );
+}
+export default ProductsPage;
