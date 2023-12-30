c0a2f52aebe8991e5d7f43a93b0832fb8fee1004
Prithibha48 <prithibhar48@gmail.com>
2023-12-30 23:21:57 +0530

    solved

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import ProductPage from './components/Productpage';
+import ProductRow from './components/ProductRow';
+import ProductTable from './components/ProductTable';
+import SearchBar from './SearchBar';
 import reportWebVitals from './reportWebVitals';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
