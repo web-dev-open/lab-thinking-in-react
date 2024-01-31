6e2c6f20bcbc2d8055e6fd32029c2f260dae3ee1
Mohammad Adnan <111442736+Mohammadadnan07@users.noreply.github.com>
2024-01-31 12:16:24 +0530

    Update ProductRow.js

--- src/component/ProductRow.js
+++ src/component/ProductRow.js
@@ -1,1 +1,20 @@
+import React from 'react'
+import './table.css'
 
+const ProductRow = ({productprop}) => {
+  return (
+
+       <tr key={productprop.id}>
+        <td style = {{ color: productprop.inStock ?
+             'black' : 'red'
+        }}>{productprop.name}</td>
+
+        <td>{productprop.price}</td>
+
+
+       </tr>
+
+  )
+}
+
+export default ProductRow
